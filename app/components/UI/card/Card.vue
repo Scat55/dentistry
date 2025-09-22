dfe6c4cca1cc03ddef7e9d3c78c418a930475e3c
Dmitry <dmit.bystroff2016@yandex.ru>
2025-09-22 18:45:31 +0300

    1126272/feat: add Card component

---
+++ app/components/UI/card/Card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="UICard">
+        <slot />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.UICard {
+    display: inline-block;
+    background-color: $white-color;
+    box-shadow: 0 0 6px -3px #000;
+    padding: 20px;
+    border-radius: 10px;
+}
+</style>
